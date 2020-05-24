190693755ab5d175f9e3028a67c8d45aa0a2894a
jeffp <jeff@email.com>
2020-05-23 22:26:16 -0600

    improved the testbench by toggling the input pin together with the clock

--- FlexibleDemux.py
+++ FlexibleDemux.py
@@ -10,14 +10,22 @@ Environment:
     Visual Studio Code
     Altera Quartus Prime 19.1 Lite Edition 
 '''
+
+# -+-+-+-+-+-+-+-+-+-+
+# -|C|o|n|s|t|a|n|t|s|
+# -+-+-+-+-+-+-+-+-+-+
+
 UNIT_TEST_ENABLE = 0
 RUN_TEST_BENCH = 1
 RUN_CONVERSION = 0
 
+# -+-+-+-+-+-+-+-+
+# -|I|m|p|o|r|t|s|
+# -+-+-+-+-+-+-+-+
+
 from myhdl import *
 import unittest
 from random import randrange
-
 
 # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 # -|M|y|H|D|L|-|U|t|i|l|i|t|i|e|s|
@@ -28,7 +36,6 @@ def getIntbV(numBit):
 
 def getSignalBool():
     return Signal(bool(0))
-
 
 # -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 # -|F|l|e|x|i|b|l|e|-|D|e|m|u|l|t|i|p|l|e|x|e|r|
@@ -62,18 +69,11 @@ def convert():
     pin_in = getSignalBool()
     toVHDL(flexibleDemux, pin_in, pins_select, pins_out)
 
-
-
 # -+-+-+-+-+-+-+-+-+-+-+
 # -|T|e|s|t|-|B|e|n|c|h|
 # -+-+-+-+-+-+-+-+-+-+-+
 
-# Test Bench example: Implementation
-
-ACTIVE_LOW = 0
-
 def testbench():
-
     clk = getSignalBool()
     pins_select = Signal(getIntbV(MUX_SEL_NUM_BIT))
     pins_out = Signal(getIntbV(MUX_NUM_BIT_OUT))
@@ -84,10 +84,11 @@ def testbench():
     @always(delay(10))
     def clkgen():
         clk.next = not clk
+        pin_in.next = not pin_in
 
     @always(clk.negedge)
     def stimulus():
-        pin_in.next = randrange(2)
+        pins_select.next = randrange(8)
 
     return flexDemuxInst, clkgen, stimulus
 
@@ -100,27 +101,14 @@ def simulate(timesteps=5000):
 # -|U|n|i|t|-|T|e|s|t|
 # -+-+-+-+-+-+-+-+-+-+
 
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
+class TestMyHDLMethods(unittest.TestCase):
     def test_intbvlength(self):
         b = getIntbV(3)
         self.assertEqual(len(b),3)
 
-
+# -+-+-+-+-+
+# -|M|a|i|n|
+# -+-+-+-+-+
 
 if UNIT_TEST_ENABLE:
     unittest.main()
